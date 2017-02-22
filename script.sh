d0fdf7deeb574ffa8c9a709dc06f8d2bbf2c7d83
JeremyLG <jeremy.le-gall@hotmail.fr>
2017-02-22 22:57:50 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,6 +1,6 @@
 echo "Placez vous dans le directory où on va placer les fichiers csv nécessaires :"
 
 wget -O airhealth.csv https://data.cityofnewyork.us/api/views/c3uy-2p5r/rows.csv?accessType=DOWNLOAD
-wget -O jobs.csv https://data.cityofnewyork.us/api/views/ic3t-wcy2/rows.csv?accessType=DOWNLOAD
+wget -O crimes.xlsx http://www.nyc.gov/html/nypd/downloads/excel/omap/nyc_park_crime_stats_q4_2014.xlsx
 wget -O housing.csv https://data.cityofnewyork.us/api/views/59kj-x8nc/rows.csv?accessType=DOWNLOAD
 wget -O activities.csv https://data.cityofnewyork.us/api/views/reni-g9vg/rows.csv?accessType=DOWNLOAD
